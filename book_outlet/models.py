41ad4e99046af310a3213c7ac6730f43d29d5db5
Gonzalo Alvarez Campos <gonzaloalvarezcampos@Gonzalos-MacBook-Pro.local>
2025-04-21 17:37:07 -0300

    author model added, author model added to admin

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,12 +5,19 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return f'Author: {self.first_name}, {self.last_name}'
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name='books')
     is_bestseller = models.BooleanField(default=False)
-    slug = models.SlugField(default='', null=False, db_index=True)
+    slug = models.SlugField(default='', blank=True, null=False, db_index=True)
     
     def __str__(self):
         return f'Title: {self.title}, Rating: {self.rating}'
